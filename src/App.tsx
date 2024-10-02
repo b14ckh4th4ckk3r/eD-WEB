d951ed7dfe9c1af3546fb2834de5b5fb06ba255a
b14ckh4th4ckk3r <vasusaini2468@gmail.com>
2024-10-02 22:50:53 +0530

    updated homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="logo">eD.WEB</div>
+        <nav>
+          <ul>
+            <li><a href="#services">Problem Set</a></li>
+            <li><a href="#portfolio">Login</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+          </ul>
+        </nav>
       </header>
+
+      <div className="background">
+        <div className="scattered-letters">
+          <span>A</span><span>PLACE</span><span>WHERE</span><span>YOU</span><span>CODE</span><span>EFFICIENTLY</span>
+        </div>
+        <main className="main-content">
+          <div className="title">eD.WEB</div>
+          {/* <p className="tagline">A Creative web Studio, that loves to make you look good</p> */}
+          <button className="about-button">About Us</button>
+        </main>
+      </div>
+
+      <footer className="footer">
+        <div className="social-links">
+          <a href="https://www.instagram.com" className="social-icon">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://www.facebook.com" className="social-icon">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://www.x.com" className="social-icon">
+            <i className="fab fa-twitter"></i>
+          </a>
+        </div>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
+
